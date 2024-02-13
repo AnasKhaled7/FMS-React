679c85b918906e7f85cf4528cfc2f9faadce6169
Anas Khaled <anas.elfayoumy7@gmail.com>
2024-02-13 18:29:42 +0200

    Add logout functionality to Sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  return (
+    <div className="d-flex flex-column align-items-center border-end border-1">
+      <p>Sidebar</p>
+
+      <button className="btn btn-danger" onClick={logout}>
+        Logout
+      </button>
+    </div>
+  );
 };
+
 export default Sidebar;
