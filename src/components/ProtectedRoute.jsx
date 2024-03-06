6e70add57d875946d57d75b423eea84b3c5f929d
Anas Khaled <anas.elfayoumy7@gmail.com>
2024-03-06 23:29:34 +0200

    Refactor ProtectedRoute and add not-found page

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,17 +1,8 @@
-import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
-import { UserContext } from "../context/UserContext";
 
-const ProtectedRoute = ({ children }) => {
-  const userData = useContext(UserContext);
-
-  return !userData && !localStorage.getItem("token") ? (
-    <Navigate to="/login" />
-  ) : (
-    children
-  );
-};
+const ProtectedRoute = ({ children }) =>
+  !localStorage.getItem("token") ? <Navigate to="/login" /> : children;
 
 ProtectedRoute.propTypes = {
   children: PropTypes.node,
--- src/modules/not-found/NotFound.jsx
+++ src/modules/not-found/NotFound.jsx
@@ -1,4 +1,34 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="not-found-container">
+      <div className="container-md d-flex flex-column gap-3 align-items-start">
+        <h2 className="fs-1">
+          Oops
+          <br />
+          <span className="text-success">Page not found</span>
+        </h2>
+
+        <p className="fs-5">
+          This Page doesn’t exist or was removed!
+          <br />
+          We suggest you back to home.
+        </p>
+
+        <Link
+          to={localStorage.getItem("token") ? "/dashboard" : "/"}
+          className="btn btn-primary btn-success d-flex gap-3 align-items-center"
+        >
+          <i
+            className={`fas ${
+              localStorage.getItem("token") ? "fa-home" : "fa-sign-in-alt"
+            }`}
+          ></i>
+          {localStorage.getItem("token") ? "Back To Home" : "Login"}
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default NotFound;
